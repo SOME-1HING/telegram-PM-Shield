3e662440d676ead783fe3f9e589a1b2db3898666
SOME1HING <yashprakash2005@gmail.com>
2022-11-21 19:48:22 +0530

    Update __main__.py

--- PmBlocker/__main__.py
+++ PmBlocker/__main__.py
@@ -1,9 +1,20 @@
+import asyncio
+
 from PmBlocker import ubot
 from PmBlocker.modules import ALL_MODULES
 from PmBlocker.logger import LOGGER
 
 
+async def load_start():
+
+    await ubot.send_message(ubot.get_me().id, "Pm Blocker Initialized")
+
+    LOGGER.info("[INFO]: STARTED")
+
+
 if __name__ == "__main__":
     ubot.start()
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    ubot.send_message(ubot.get_me().id, "Pm Blocker Initialized")
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    loop.run_until_complete(load_start())
+    loop.close()
