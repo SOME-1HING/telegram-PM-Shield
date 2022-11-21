9f836536648680cd616999caec9a593f24e56b87
SOME1HING <yashprakash2005@gmail.com>
2022-11-21 20:13:50 +0530

    Update __main__.py

--- PmBlocker/__main__.py
+++ PmBlocker/__main__.py
@@ -5,14 +5,6 @@ from PmBlocker.modules import ALL_MODULES
 from PmBlocker.logger import LOGGER
 
 
-async def load_start():
-
-    await ubot.send_message(UB_ID, "Pm Blocker Initialized")
-
-    return
-
-
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     LOGGER.info("[INFO]: STARTED")
-    ubot.loop.run_until_complete(load_start())
